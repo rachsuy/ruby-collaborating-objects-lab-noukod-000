5b5d4354c29c85976d1fff5576ebff3a868c855b
Rachele Suy <rachelesuy@gmail.com>
2019-05-17 13:43:06 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,8 @@ class MP3Importer
   end
   
   def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+    list_of_filenames.each do |e| Song.new_by_filename(e) 
+  end 
   end
   
   def files
